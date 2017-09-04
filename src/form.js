0c4f04c2cb63cd8f46833a9743913fa0d3b9c8de
Joseph Luck <joseph.luck@sky.com>
2017-09-04 18:05:19 +0100

    Add form and list-form models

---
+++ src/form.js
@@ -0,0 +1,108 @@
+import Core from 'core'
+import validate from 'validation/validator'
+import { Err, Ok, Result } from 'space-lift'
+
+export function model({
+  constraints,
+  defaultForm,
+}) {
+  const fields = defaultForm()
+  const emptyErrors = makeDefaultErrors(constraints(fields))
+
+  function initialState() {
+    return {
+      fields,
+      errors: emptyErrors,
+      valid: false,
+      formShowing: null,
+    }
+  }
+
+  return {
+    scoped: true,
+    state: initialState(),
+    reducers: {
+      resetForm() {
+        return initialState()
+      },
+      initFields(state, fields) {
+        return { fields: Object.assign({}, state.fields, fields) }
+      },
+      setFields(state, newFields) {
+        const fields = Object.assign({}, state.fields, newFields)
+        const newErrors = getErrorsForFields(newFields, constraints)
+        const errors = Object.assign({}, state.errors, newErrors)
+        const valid = isFormValid(fields, constraints)
+        return { fields, errors, valid }
+      },
+      validateEntireForm(state) {
+        const errors = validate(state.fields, constraints(state.fields))
+        return {
+          ...state,
+          errors: errors || emptyErrors,
+          valid: isFormValid(state.fields, constraints),
+        }
+      },
+      validateFields(state, fields) {
+        const fieldsToValidate = fields.reduce((prev, key) => {
+          return {
+            ...prev,
+            [key]: state.fields[key],
+          }
+        }, {})
+        const newErrors = getErrorsForFields(fieldsToValidate, constraints)
+        const errors = Object.assign({}, state.errors, newErrors)
+        return {
+          errors,
+          valid: isFormValid(state.fields, constraints),
+        }
+      },
+      toggleFormShowing(state, { name }) {
+        return name === state.formShowing ? { formShowing: null } : { formShowing: name }
+      },
+    },
+    effects: {
+      validateOnSubmit(_state, send) {
+        const state = send.validateEntireForm()
+        if (!state.valid) {
+          return Err({
+            type: 'validation_error',
+            message: 'Invalid input',
+            errors: state.errors,
+          })
+        } else {
+          return Ok(state)
+        }
+      },
+    },
+  }
+}
+
+export function makeDefaultErrors(constraints) {
+  const errors = Object.keys(constraints).reduce((prev, curr) => {
+    return {
+      ...prev,
+      [curr]: [],
+    }
+  }, {})
+  return errors
+}
+
+export function getErrorsForFields(fields, makeConstraints) {
+  const constraints = makeConstraints(fields) || {}
+  const keys = Object.keys(fields)
+  const initialErrors = keys.reduce((prev, key) => ({ ...prev, [key]: [] }), {})
+
+  const filteredConstraints = keys.reduce((prev, key) => {
+    return constraints[key] ? { ...prev, [key]: constraints[key] } : prev
+  }, {})
+  const errors = validate(fields, filteredConstraints) || {}
+  return { ...initialErrors, ...errors }
+}
+
+export function isFormValid(fields, makeConstraints) {
+  const errors = getErrorsForFields(fields, makeConstraints)
+  return Object.keys(errors).reduce((prev, key) => {
+    return prev && !errors[key].length
+  }, true)
+}
---
+++ src/list-form.js
@@ -0,0 +1,97 @@
+import * as identity from 'lodash/fp/identity'
+import * as Form from 'models/form'
+import { Models } from 'models'
+import replace from 'utils/replace-array-element'
+
+export function model({
+  constraints,
+  defaultForm,
+  sanitize = identity,
+  deSanitize = identity,
+}) {
+  const form = Form.model({ constraints, defaultForm })
+  const defaultState = () => {
+    return {
+      ...form.state,
+      formVisible: false,
+      items: [],
+      currentlyEditingIndex: null,
+    }
+  }
+  return {
+    scoped: true,
+    state: defaultState(),
+    reducers: {
+      ...form.reducers,
+      resetState: defaultState,
+      setItems(_state, items) {
+        return { items }
+      },
+      addItem(state, { item }) {
+        return {
+          items: state.items.concat(item),
+        }
+      },
+      updateItem(state, { index, item }) {
+        return {
+          items: replace(state.items, item, index),
+        }
+      },
+      removeItem(state, { index }) {
+        const fields = defaultForm()
+        const formConstraints = constraints(fields)
+        return {
+          items: state.items.filter((_item, i) => i !== index),
+          currentlyEditingIndex: null,
+          fields: fields,
+          errors: Form.makeDefaultErrors(formConstraints),
+        }
+      },
+      setFormVisibility(_state, { visible }) {
+        return {
+          formVisible: visible,
+        }
+      },
+      setEditingItem(_state, { index }) {
+        return {
+          currentlyEditingIndex: index,
+        }
+      },
+    },
+    effects: {
+      ...form.effects,
+      submitForm(state, send) {
+        return send.validateOnSubmit().fold(
+          () => Promise.resolve(state),
+          () => {
+            if (state.currentlyEditingIndex !== null) {
+              send.updateItem({
+                index: state.currentlyEditingIndex,
+                item: sanitize(state.fields),
+              })
+            } else {
+              send.addItem({
+                item: sanitize(state.fields),
+              })
+            }
+            send.setFormVisibility({ visible: false })
+            send.setEditingItem({ index: null })
+            const last = send.resetForm()
+            return Promise.resolve(last)
+          })
+      },
+      beginEditingItem(state, send, { index }) {
+        send.setEditingItem({ index })
+        send.setFields(deSanitize(state.items[index]))
+        const last = send.setFormVisibility({ visible: true })
+        return Promise.resolve(last)
+      },
+      discardEditingItem(_state, send) {
+        send.setFormVisibility({ visible: false })
+        send.resetForm()
+        const last = send.setEditingItem({ index: null })
+        return Promise.resolve(last)
+      },
+    },
+  }
+}
